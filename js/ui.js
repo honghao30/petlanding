59b9da9dc748c564deddfef8f08a8469da8bb5b2
hongsh <hongsh@the-51.com>
2024-03-21 08:30:00 +0900

    탭

--- js/ui.js
+++ js/ui.js
@@ -34,16 +34,56 @@ commonButton && commonButton.addEventListener('click', (e) => {
 
 // 드랍다운
 const selectedOptionButton = document.querySelector('.selected-option');
+const optionList = document.querySelectorAll('.option-list li button');
 
-selectedOptionButton.addEventListener('click', function() {
+selectedOptionButton.addEventListener('click', function() {    
     selectedOptionButton.classList.toggle('is-active');
+    selectedOptionButton.parentElement.classList.toggle('is-active');
     selectedOptionButton.nextElementSibling.classList.toggle('is-active');    
 });
-
+optionList.forEach(option => {
+    option.addEventListener('click', () => {
+        const selectedValue = option.getAttribute('data-option');
+        selectedOptionButton.textContent = selectedValue;
+        
+        const allOptionItems = document.querySelectorAll('.option-list li');
+        allOptionItems.forEach(item => {
+            if (item !== option.parentElement) {
+                item.classList.remove('is-active');
+            }
+        });
+        option.parentElement.classList.add('is-active');
+        selectedOptionButton.classList.toggle('is-active');
+        selectedOptionButton.parentElement.parentElement.classList.toggle('is-active');
+        selectedOptionButton.nextElementSibling.classList.toggle('is-active');            
+    });
+})
 // 영역 외를 클릭하면 드랍다운 닫기
-document.addEventListener('click', function(event) {
-    if (!event.target.closest('.filter-option')) {
-        selectedOptionButton.classList.remove('is-active');   
-        selectedOptionButton.nextElementSibling.classList.remove('is-active');       
+document.addEventListener("click", function(e) {
+    if (document.querySelector('.fillter-option.is-active') && !e.target.closest('.fillter-option')) {
+        document.querySelector('.fillter-option.is-active').classList.remove('is-active');
+        selectedOptionButton.classList.remove('is-active');        
+        selectedOptionButton.nextElementSibling.classList.remove('is-active');    
+
     }
 });
+
+//tabMenu
+const tabButtons = document.querySelectorAll('.tab-area .btn-tab');
+const tabList = document.querySelector('.tab-area ul');
+
+tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const clickedLi = button.parentElement;
+        const isActive = clickedLi.classList.contains('is-active');
+
+        if (!isActive) {
+            tabList.querySelector('.is-active').classList.remove('is-active');
+            clickedLi.classList.add('is-active');
+        }        
+    });
+});
+
+const calcTabWidth = () => {
+    
+}
