9d9d22a879dc2a85d67c26322e67223ef0a49440
hongsh <hongsh@the-51.com>
2024-03-20 09:51:37 +0900

    모달창

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,18 @@
-console.log('로딩')
+// 토글 기능
+const toggleBtns = document.querySelectorAll('.toggle-btn');
+toggleBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('is-active');
+    });
+});
+// 공유
+const snsShare = () => {
+    const modalLayer = document.querySelector('.sns-share');
+    modalLayer.classList.add('is-active');
+}
+
+// 모달 닫기
+const closeButton = document.querySelector('.modal-close');
+closeButton && closeButton.addEventListener('click', () => {
+    closeButton.parentElement.parentElement.classList.remove('is-active');
+})
